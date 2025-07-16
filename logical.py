252fab6aca91ad060db5570aa3d86d518ceb7bb7
Godiya1 <godiyajoshua2023@gmail.com>
2025-07-16 12:38:59 +0000

    first commit

---
+++ logical.py
@@ -0,0 +1,72 @@
+first_number = 0
+second_number = 6
+# or
+# operand
+# operator
+'''
+if first_number or second_number
+print("valid operation")
+
+
+first_number = 4
+second_number = 5
+# Arimetic operatiors
+'''
+"""
+# A simple calculator app
+print('''1. Addition
+2. Subtraction
+3. multiplication
+4. Division
+''')
+print("Enter two numbers to add")
+#promt a user for a number and store it
+first_number = input("First_number:")
+#promt a user for a number and store it
+second_number = input("second_number:")    
+sum = float(first_number) + float(second_number)
+print(f"{first_number} + {second_number} = {sum}")
+
+
+print("*********")
+print("subtraction")
+print("Enter two numbers to subtract")
+#promt a user for a number and store it
+first_number = input("First_number:")
+#promt a user for a number and store it
+second_number = input("second_number:")
+sub = float(first_number) - float(second_number)
+print(f"{first_number} + {second_number} = {sub}")
+"""
+print('''*********
+multiplication''')
+print("Enter two numbers to multiply")
+#promt a user for a number and store it
+first_number = input("First_number:")
+#promt a user for a number and store it
+second_number = input("second_number:")
+mul = float(first_number) * float(second_number)
+print(f"{first_number} * {second_number} = {mul:,.2f}") 
+  
+  
+print('''*********
+exponetial''')
+print("Enter two numbers to exponent")
+#promt a user for a number and store it
+first_number = input("First_number:")
+#promt a user for a number and store it
+second_number = input("second_number:")
+expo = float(first_number) ** float(second_number)
+print(f"{first_number} ** {second_number} = {expo:,.2f}")
+
+
+
+
+
+
+
+
+
+
+
+
