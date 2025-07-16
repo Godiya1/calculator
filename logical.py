2d14f8c0cfccaf3c03ef9092cc7103c55319f205
Godiya1 <godiyajoshua2023@gmail.com>
2025-07-16 18:51:15 +0000

    division, floor division and class work

--- logical.py
+++ logical.py
@@ -1,9 +1,10 @@
+'''
 first_number = 0
 second_number = 6
 # or
 # operand
 # operator
-'''
+
 if first_number or second_number
 print("valid operation")
 
@@ -12,12 +13,15 @@ first_number = 4
 second_number = 5
 # Arimetic operatiors
 '''
-"""
+
 # A simple calculator app
-print('''1. Addition
+print('''
+1. Addition
 2. Subtraction
 3. multiplication
 4. Division
+5. Exponential
+6. floor division
 ''')
 print("Enter two numbers to add")
 #promt a user for a number and store it
@@ -37,7 +41,8 @@ first_number = input("First_number:")
 second_number = input("second_number:")
 sub = float(first_number) - float(second_number)
 print(f"{first_number} + {second_number} = {sub}")
-"""
+
+
 print('''*********
 multiplication''')
 print("Enter two numbers to multiply")
@@ -48,7 +53,17 @@ second_number = input("second_number:")
 mul = float(first_number) * float(second_number)
 print(f"{first_number} * {second_number} = {mul:,.2f}") 
   
-  
+print('''*********
+division''')
+print("Enter two numbers to division")
+#promt a user for a number and store it
+first_number = input("First_number:")
+#promt a user for a number and store it
+second_number = input("second_number:")
+div = float(first_number) / float(second_number)
+print(f"{first_number} / {second_number} = {div:,.2f}") 
+
+
 print('''*********
 exponetial''')
 print("Enter two numbers to exponent")
@@ -60,9 +75,58 @@ expo = float(first_number) ** float(second_number)
 print(f"{first_number} ** {second_number} = {expo:,.2f}")
 
 
+print('''*********
+floor division''')
+print("Enter two numbers to floor division")
+#promt a user for a number and store it
+first_number = input("First_number:")
+#promt a user for a number and store it
+second_number = input("second_number:")
+floor_division = float(first_number) // float(second_number)
+print(f"{first_number} // {second_number} = {floor_division:,.2f}")
+ 
+
+'''
+first_number = 4
+first_number = first_number
+print(first_number)
+
+first_number = 5
+first_number +=3
+print(first_number)
+
+first_number = 7
+first_number *=5
+print(first_number
+'''
 
 
 
+
+# CLASS WORK
+allowance = 2000
+allowance -=400
+print(allowance)
+
+allowance +=100
+print(allowance)
+
+allowance -=250
+print(allowance)
+        
+percent = 25 / 100
+percent = allowance * 25 / 100
+allowance -= percent
+print(f"Allowance: {allowance}")
+
+third = allowance / 3
+allowance -= third
+print(allowance)
+
+allowance //=2
+print(allowance)
+allowance %=100
+print(allowance)
 
 
 
